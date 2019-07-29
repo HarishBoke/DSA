7fd8c874b8d4bc7eb215bfe07ae51515de5008f8
Harish Boke <harish.boke@gmail.com>
2019-07-29 12:10:10 +0530

    Queue DS revise start

---
+++ queue_ds.js
@@ -0,0 +1,12 @@
+/* A queue is a first-in, first-out data structure (FIFO). 
+We can only remove items from the queue one at a time, and must remove items in the same sequence as they were placed in the queue.*/
+
+//using factory function
+
+function createQueue(){
+    // add or queue
+    // remove or dequeue
+    // peek
+    // length
+    // isEmpty
+}
