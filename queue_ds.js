4418983fe53d176e1258dda6d04de796cb3734b2
Harish Boke <harish.boke@gmail.com>
2019-07-30 20:18:02 +0530

    added queue DS logic with Javascript

--- queue_ds.js
+++ queue_ds.js
@@ -2,11 +2,50 @@
 We can only remove items from the queue one at a time, and must remove items in the same sequence as they were placed in the queue.*/
 
 //using factory function
-
-function createQueue(){
     // add or queue
     // remove or dequeue
     // peek
     // length
     // isEmpty
+
+function createQueue(){
+    const queue =[];
+    return{
+        enqueue(x){
+            queue.unshift(x);
+        },
+        dequeue(){
+            if(queue.length === 0){
+                return undefined;
+            }
+            queue.pop();
+        },
+        peek(){
+            if(queue.length === 0){
+                return undefined;
+            }
+            return queue[queue.length-1];
+        },
+        get length(){
+            return queue.length;
+        },
+        isEmpty(){
+            return queue.length === 0;
+        }
+    }
 }
+
+const q = createQueue();
+
+console.log("Is queue Empty?: ", q.isEmpty());
+
+q.enqueue('first')
+q.enqueue('second')
+q.enqueue('third')
+
+console.log('queue length: ', q.length);
+
+console.log('queue peek element: ', q.peek());
+
+
+
