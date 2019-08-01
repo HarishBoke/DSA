70b9fb38242551e641e15734da73642dfee4e79b
Harish Boke <harish.boke@gmail.com>
2019-08-01 19:56:23 +0530

    Linked list DS

---
+++ linkedList_ds.js
@@ -0,0 +1,23 @@
+// linked list in using factory function
+
+
+function createNode(value){
+    return {
+        value,
+        next: null
+    }
+}
+function createLinkedList(){
+    // ** prop 
+    //head
+    //tail
+    // length
+
+
+    // ** methods
+    // push
+    // pop
+    // delete
+    // isEmpty 
+
+}
