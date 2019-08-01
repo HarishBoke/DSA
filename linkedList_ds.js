b5747e5a1955b0428343a3827009058de32ba8a4
Harish Boke <harish.boke@gmail.com>
2019-08-01 23:54:38 +0530

    Linked list DS - added base methods and props

--- linkedList_ds.js
+++ linkedList_ds.js
@@ -8,12 +8,37 @@ function createNode(value){
     }
 }
 function createLinkedList(){
+
+    return {
+        head: null,
+        tail: null,
+        length: 0,
+        push(value){
+            var node = createNode(value);
+            if(node.next == null){
+                
+                this.head = null;
+                this.tail = null;
+                this.length = 0;
+            }
+        },
+        pop(){
+
+        },
+        get(){
+
+        },
+        delete(){
+
+        },
+        isEmpty(){
+            return this.length === 0;
+        }
+    }
     // ** prop 
     //head
     //tail
     // length
-
-
     // ** methods
     // push
     // pop
