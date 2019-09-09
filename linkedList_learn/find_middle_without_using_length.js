a0916a5f42b2a821cc9b75a7ad1c9e1dc8439ff9
Harish Boke <harish.boke@gmail.com>
2019-09-10 05:28:22 +0530

    added improovised  solution it find middle

--- linkedList_learn/find_middle_without_using_length.js
+++ linkedList_learn/find_middle_without_using_length.js
@@ -1,8 +1,9 @@
-// Assuming you know array find middle of of list /array without using length prop
+// Assuming you know array find middle of of list 
 
-const arr = [1,2,4,5,7,8,9,0];
+const arr = [1,2,4,5,7,8,9,0,2];
 
 // 1. navie
+//condition: without using length prop
 function findMiddle(){
     let i = 0;
     let middle;
@@ -12,6 +13,24 @@ function findMiddle(){
     if(arr[i] == undefined){
         middle = Math.ceil(i /2);
     }
-    console.log(`middle of the given array is ${middle}`)
+    console.log(`middle of the given array is ${middle} using Naive solution`);
 }
 findMiddle();
+
+// 2. improovised
+//condition: 1. without using length prop 2. without using divided / 2
+
+const arr1 = [1,2,4,5,7,8,9,0,1];
+function findMiddle1(){
+    let i =0;
+    let middle;
+    while(arr1[i] != undefined){
+        if(arr1[i * 2] != undefined){
+            // as it is checking on less element added 1 manually
+            middle = i+1;
+        }
+        i++;
+    }
+    console.log(`middle of the given array is ${middle} using improovised solution`);
+}
+findMiddle1();
