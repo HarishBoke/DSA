450c5d0b97a44be582ee8f347493f6c392d00a7e
Harish Boke <harish.boke@gmail.com>
2019-09-16 21:47:34 +0530

    updated code and naming conventions

--- binarySearch/binarySearch_recurssive.js
+++ binarySearch/binarySearch_recurssive.js
@@ -8,28 +8,33 @@
 // if start > end = element not found
 let arrI = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59];
 let startI = 0;
-let endI = exampleArray.length-1;
+let endI = arrI.length-1;
 let mid = (startI+endI) / 2;
-let targetI = 17;
+let targetI = 37;
 
 function findElement(arr, target, start, end){
         // assumed arr.length > 0
-        mid = Math.floor((start + end) / 2);
-        if(arr[mid] === t){
-            return mid;
-        }
-        else if(arr[mid] > target){
-            end = mid-1;
-             return findElement(arr, target, start, end);
-        }
-        else if(arr[mid] < target){
-            start = mid+1;
-            return findElement(arr, target, start, end);
-        }
+        if(start > end){ return -1 }
         else {
-            return -1;
-            // not found
+            mid = Math.floor((start + end) / 2);
+            if(arr[mid] === target){
+                return mid;
+            }
+            else if(target < arr[mid]){
+                end = mid-1;
+                 return findElement(arr, target, start, end);
+            }
+            else if(target > arr[mid]){
+                start = mid+1;
+                return findElement(arr, target, start, end);
+            }
+            else {
+                return -1;
+                // not found
+            }
         }
+            
+
 }
 
 console.log(`Target found at ${findElement(arrI, targetI, startI, endI)}`);
