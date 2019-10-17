bcc1d31dbefcd2a6ea67e8b2b1366910819ffe49
Harish Boke <harish.boke@gmail.com>
2019-10-17 23:02:21 +0530

    removed JS specific syntac as default push method removed

--- mergeSort/TwoWayMergeSort-Iterative-method.js
+++ mergeSort/TwoWayMergeSort-Iterative-method.js
@@ -12,29 +12,34 @@ function merge(first, second, firstLength, secondLength){
     // comparing and copying elements
     while(i < firstLength && j < secondLength){
         if(first[i] < second[j]){
-            //mergedList.push(first[i]);
-           mergedList[k] = first[i];
-            i++;
-            k++;
+            //1. //mergedList.push(first[i]);
+        //2. //    mergedList[k] = first[i];
+        //     i++;
+        //     k++;
+        //3. //
+        mergedList[k++] = first[i++];
         }
         else {
             //mergedList.push(second[j]);
-            mergedList[k] = second[j];
-            j++;
-            k++;
+            // mergedList[k] = second[j];
+            // j++;
+            // k++;
+            mergedList[k++] = first[j++];
         }
     }
     // copy remaining first list
     for(; i< firstLength; i++){
         //mergedList.push(first[i]);
-        mergedList[k] = first[i];
-        k++;
+        // mergedList[k] = first[i];
+        // k++;
+        mergedList[k++] = first[i++];
     }
     // copy remaining second list
     for(; j < secondLength; j++) {
        // mergedList.push(second[j]);
-       mergedList[k] = second[j];
-       k++;
+    //    mergedList[k] = second[j];
+    //    k++;
+    mergedList[k++] = first[j++];
     }
 
     return mergedList;
