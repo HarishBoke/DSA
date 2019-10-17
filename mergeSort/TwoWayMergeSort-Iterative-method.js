51c417acdd40cc57ff721f2106a027026015e475
Harish Boke <harish.boke@gmail.com>
2019-10-17 23:06:14 +0530

    added increment shortcut

--- mergeSort/TwoWayMergeSort-Iterative-method.js
+++ mergeSort/TwoWayMergeSort-Iterative-method.js
@@ -13,18 +13,18 @@ function merge(first, second, firstLength, secondLength){
     while(i < firstLength && j < secondLength){
         if(first[i] < second[j]){
             //1. //mergedList.push(first[i]);
-        //2. //    mergedList[k] = first[i];
-        //     i++;
-        //     k++;
-        //3. //
-        mergedList[k++] = first[i++];
+            //2. //    mergedList[k] = first[i];
+            //     i++;
+            //     k++;
+            //3. //
+            mergedList[k++] = first[i++];
         }
         else {
             //mergedList.push(second[j]);
             // mergedList[k] = second[j];
             // j++;
             // k++;
-            mergedList[k++] = first[j++];
+            mergedList[k++] = second[j++];
         }
     }
     // copy remaining first list
@@ -32,14 +32,14 @@ function merge(first, second, firstLength, secondLength){
         //mergedList.push(first[i]);
         // mergedList[k] = first[i];
         // k++;
-        mergedList[k++] = first[i++];
+        mergedList[k++] = first[i];
     }
     // copy remaining second list
     for(; j < secondLength; j++) {
-       // mergedList.push(second[j]);
-    //    mergedList[k] = second[j];
-    //    k++;
-    mergedList[k++] = first[j++];
+        // mergedList.push(second[j]);
+        //    mergedList[k] = second[j];
+        //    k++;
+        mergedList[k++] = second[j];
     }
 
     return mergedList;
