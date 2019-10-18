d804631f5ec89a4a8e46878dc3f773668512d665
Harish Boke <harish.boke@gmail.com>
2019-10-18 23:09:20 +0530

    sort and merge at first attempt

---
+++ mergeSort/mergeSort_learning.js
@@ -0,0 +1,20 @@
+// MergeSort(arr[], left,  right):
+// If right > l:
+//      1. Find the middle point to divide the array into two halves:
+//              mid = (left+right)/2
+//      2. Call mergeSort for first half:
+//              Call mergeSort(arr, left, mid)
+//      3. Call mergeSort for second half:
+//              Call mergeSort(arr, mid+1, right)
+//      4. Merge the two halves sorted in step 2 and 3:
+//              Call merge(arr, left, mid, right)
+
+function mergeSort(arr, start, end){
+    if(end > 1) return 
+
+}
+
+
+function stich(){
+
+}
